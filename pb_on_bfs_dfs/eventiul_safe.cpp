71d2cec3bd9cac8713df00dc088ebc7895a28eed
Omkar Potdar <ompotdar7498@gmail.com>
2026-08-02 22:22:53 +0530

    Finish eventual safe states

--- pb_on_bfs_dfs/eventiul_safe.cpp
+++ pb_on_bfs_dfs/eventiul_safe.cpp
@@ -1,17 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+    vector<int> eventualSafeNodes(vector<vector<int>> &graph)
+    {
         // reverse the edges
         int V = graph.size();
         vector<int> adjRev[V];
         vector<int> safenodes;
         vector<int> indegree(V, 0);
 
-        for (int i = 0; i < V; i++) {
-            for (auto it : graph[i]) {
+        for (int i = 0; i < V; i++)
+        {
+            for (auto it : graph[i])
+            {
 
                 adjRev[it].push_back(i);
                 indegree[i]++;
@@ -19,17 +23,21 @@ public:
         }
 
         queue<int> q;
-        for (int i = 0; i < V; i++) {
-            if (indegree[i] == 0) {
+        for (int i = 0; i < V; i++)
+        {
+            if (indegree[i] == 0)
+            {
                 q.push(i);
             }
         }
 
-        while (!q.empty()) {
+        while (!q.empty())
+        {
             int node = q.front();
             q.pop();
             safenodes.push_back(node);
-            for (auto it : adjRev[node]) {
+            for (auto it : adjRev[node])
+            {
                 indegree[it]--;
                 if (indegree[it] == 0)
                     q.push(it);
@@ -37,6 +45,17 @@ public:
         }
         sort(safenodes.begin(), safenodes.end());
         return safenodes;
+    }
+};
+
+// do using cycle detection
+
+class Solution2
+{
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>> &graph)
+    {
+
     }
 };
 
